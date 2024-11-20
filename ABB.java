2e18b36771f5d0aa1fffba390d352dae8b4b2bab
jimeeeb08 <129323337+jimeeeb08@users.noreply.github.com>
2024-11-20 00:45:53 -0600

    primer avance

---
+++ ABB.java
@@ -0,0 +1,24 @@
+class Nodo{
+    int valor;
+    Nodo izq;
+    Nodo der;
+    
+    //Constructor
+    public Nodo (int valor){
+        this.valor = valor;
+        this.izq = null;
+        this.der = null;
+    }
+}
+
+//árbol
+public class ABB {
+    private Nodo raiz;
+    
+    // Constructor
+    public ABB() {
+         this.raiz = null;
+    }
+    
+    //insertar nodos....
+}
