fc72d76ddfd517e6cd19c677e23a5c1052f12e82
Ignacio Jose Calero Villalobos <ignacio.calero@ucr.ac.cr>
2024-11-29 08:30:28 -0600

    Se creo la clase main

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Clase principal que inicia la aplicación
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal principal = new VentanaPrincipal(); // Crea la ventana principal
+        principal.setVisible(true); // Muestra la ventana
+    }
+}
